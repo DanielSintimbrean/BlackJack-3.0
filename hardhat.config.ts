22db325298ddc6c41a23f99f0f3b8cfb7365d86d
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-08-17 23:13:20 +0200

    Add hardhat-trace

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
+import "hardhat-tracer";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
